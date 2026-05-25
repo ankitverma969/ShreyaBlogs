87271419fab94ee6f9233e0e5cbde869a28a2243
ankitverma969 <hpvankitverma1@gmail.com>
2026-05-25 11:34:35 +0530

    Update SW cache version to clear bad icon cache

--- client/public/sw.js
+++ client/public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'shreya-writes-v1';
+const CACHE_NAME = 'shreya-writes-v2';
 const STATIC_ASSETS = ['/', '/manifest.json', '/robots.txt'];
 
 self.addEventListener('install', (event) => {
